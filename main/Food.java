aa401ca369423d9d639395428090039441ba0653
Isa Alif <malif@uwo.ca>
2025-03-16 16:56:45 -0400

    Added item interface, and a toy and food class implementing this, has no comments for now

---
+++ main/Food.java
@@ -0,0 +1,71 @@
+class Food implements Item {
+    private double item_price;
+    private int number_item;
+    private int item_max;
+    private int type;
+    private String label;
+    private double hunger_bonus;
+
+    public Food(int type, int number_item){
+        switch(type){
+            case 1: 
+                this.item_price = 10;
+                this.number_item = number_item;
+                this.item_max = 100;
+                this.label = "Apple";
+                this.hunger_bonus = 10;
+            break;
+            case 2: 
+                this.item_price = 15;
+                this.number_item = number_item;
+                this.item_max = 100;
+                this.label = "Banana";
+                this.hunger_bonus = 12;
+            break;
+            default:
+                this.item_price = 0;
+                this.number_item = 0;
+                this.item_max = 0;
+                this.label = "";
+                this.hunger_bonus = 0;
+        }
+    }
+
+    private double get_hunger_bonus(){
+        return hunger_bonus;
+    }
+
+    @Override
+    private String get_label(){
+        return label;
+    }
+    @Override
+    private int get_type(){
+        return type;
+    }
+    @Override 
+    private void decrease_item(){
+        if (number_item > 0){
+            this.number_item--;
+        }
+    }
+    @Override 
+    private double get_price(){
+        return item_price;
+    }
+    @Override 
+    private int get_number_item(){
+        return number_item;
+    }
+    @Override 
+    private int get_item_max(){
+        return item_max;
+    }
+    @Override 
+    private void increase_item(int bought){
+        if ((number_item + bought) <= item_max){
+            this.number_item += bought;
+        }
+    }
+
+}
